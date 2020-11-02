752a2a17338b41ccd9631903b02d4754a4359d80
igorp-lopes <igorp.lopes16@gmail.com>
2020-11-02 12:47:48 -0300

    Criada base da função de determinar as ocorrencias dos números sorteados

--- helpers/findRecurrency.py
+++ helpers/findRecurrency.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
 
-def findRecurrency(parameter_list):
+def findRecurrency(dataframe):
     """
     Function used to find how many times each number has been selected as
     a winning number in a given dataframe with the results
     """
 
-    pass
+    # We select only the drawn numbers from the dataframe
+    dfNumbers = dataframe.iloc[:, 2:]
+
+    return dfNumbers
