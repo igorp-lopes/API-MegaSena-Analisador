b51179d84d6a99552b3661925c990d72101232f4
igorp-lopes <igorp.lopes16@gmail.com>
2020-11-02 11:08:17 -0300

    Ajustada a função para retornar o subset do dataframe

--- helpers/selectDateInterval.py
+++ helpers/selectDateInterval.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 
 
-def selectDateInterval(datesSeries, startDate):
+def selectDateInterval(dataframe, startDate):
     """
     Function used to slice a subset of the results dataframe using a given date 
     to specify where the subset ends
@@ -12,10 +12,14 @@ def selectDateInterval(datesSeries, startDate):
     startDate = datetime.strptime(startDate, "%d/%m/%Y")
     startDate = startDate.date()
 
+    # From the dataframe we extract the columns relative to the dates
+    datesSeries = dataframe["Data do Sorteio"]
+
     # We convert our date column of the dataframe to datetime
-    datesSeries = pd.to_datetime(datesSeries)
+    datesSeries = pd.to_datetime(datesSeries, format="%d/%m/%Y")
     datesSeries = datesSeries.dt.date
 
+    # We create a mask to select only the rows whose date are older than the start date
     selectionMask = (datesSeries >= startDate)
 
-    return selectionMask
+    return dataframe[selectionMask]
