0fc5440a44bbe886351bf1b4f45ab091e425c104
igorp-lopes <igorp.lopes16@gmail.com>
2020-11-23 01:06:02 -0300

    Criada base da API

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
-from helpers import extractData
+from fastapi import FastAPI
 
-extractData.extractData()
+from helpers.saveToCsv import saveToCsv
+from helpers.saveToJson import saveToJson
+from helpers.readFromCsv import readFromCsv
+from helpers.extractData import extractData
+from helpers.selectDateInterval import selectDateInterval
+from helpers.findRecurrency import findRecurrency
+from helpers.findDates import findNewest, findOldest
+
+app = FastAPI()
+
+baseDf = readFromCsv('resources/DadosMegasena.csv')
+
+
+@app.get("/recurrency")
+async def getRecurrency():
+
+    recurrencyDf = findRecurrency(baseDf)
+    json = saveToJson(recurrencyDf)
+
+    return json
