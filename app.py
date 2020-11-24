8f8e83ab06dc46cfbdab2c889474aa571a0601a3
Igor <igorp.lopes16@gmail.com>
2020-11-23 22:19:41 -0300

    Feita a route que entrega o total de recorrencia de cada numero

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 from helpers.saveToCsv import saveToCsv
 from helpers.saveToJson import saveToJson
@@ -14,9 +15,14 @@ baseDf = readFromCsv('resources/DadosMegasena.csv')
 
 
 @app.get("/recurrency")
-async def getRecurrency():
+async def getRecurrency(startDate: Optional[str] = None):
+    # If an starting date is given, select the subset of the dataframe
+    if startDate:
+        tempDf = selectDateInterval(baseDf, startDate)
+    else:
+        tempDf = baseDf
 
-    recurrencyDf = findRecurrency(baseDf)
+    recurrencyDf = findRecurrency(tempDf)
     json = saveToJson(recurrencyDf)
 
     return json
