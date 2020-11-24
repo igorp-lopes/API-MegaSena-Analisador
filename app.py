c0adb96af20bf4c473d9a696cb94b0f02a041bf4
Igor <igorp.lopes16@gmail.com>
2020-11-24 00:13:50 -0300

    Feita a route que entrega todos os resultados dos sorteios sem processamento

--- app.py
+++ app.py
@@ -14,6 +14,24 @@ app = FastAPI()
 baseDf = readFromCsv('resources/DadosMegasena.csv')
 
 
+@app.get("/results")
+async def getResults(startDate: Optional[str] = None):
+    '''
+    Route to get the unprocessed results of all the contests or in a
+    subset of contests starting from a given date up to the latest contest
+    '''
+
+    # If an starting date is given, select the subset of the dataframe
+    if startDate:
+        tempDf = selectDateInterval(baseDf, startDate)
+    else:
+        tempDf = baseDf
+
+    json = saveToJson(tempDf)
+
+    return json
+
+
 @app.get("/recurrency")
 async def getRecurrency(startDate: Optional[str] = None):
     '''
