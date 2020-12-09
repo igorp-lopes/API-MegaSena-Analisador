70540d511755723eae72097a8dc4707de02d83af
Igor P. Lopes <igorplopes@pop-os.localdomain>
2020-12-09 00:18:34 -0300

    Reorganizada as funções auxiliares/de análise

--- app/helpers/getData.py
+++ app/helpers/getData.py
@@ -1,6 +1,34 @@
+from zipfile import ZipFile
 from bs4 import BeautifulSoup
-from . import downloadData
 import pandas as pd
+import requests
+import pathlib
+import os
+
+
+def obtainData():
+
+    # We obtain the main directory of the program
+    rootDir = pathlib.Path.cwd()
+
+    # We obtain the directory where the data will be downloaded
+    resourcesDir = rootDir.joinpath('resources')
+
+    databasePath = resourcesDir.joinpath('DadosMegasena.html')
+
+    # Website of the archive with the data
+    url = "http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0K8DBC0QPVN93KQ10G1/res/id=historicoHTML/c=cacheLevelPage/=/"
+
+    try:
+        # Object given by the request
+        response = requests.get(url)
+
+    except Exception as error:
+        print("\n The following exception occurred when downloading the data:\n", error)
+
+    else:
+
+        return response
 
 
 def extractData():
@@ -10,7 +38,7 @@ def extractData():
     """
 
     # We obtain the html with the data
-    responseHtml = downloadData.obtainData()
+    responseHtml = obtainData()
 
     # We parse the html to an BeautifulSoup object
     soup = BeautifulSoup(responseHtml.content, "html5lib")
--- app/routers/results.py
+++ app/routers/results.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 from typing import Optional
 
+from app.helpers import dataframeAnalysis
+from app.helpers import auxiliary
+
 router = APIRouter()
 
 
@@ -10,13 +13,14 @@ async def getResults(startDate: Optional[str] = None):
     Route to get the unprocessed results of all the contests or in a
     subset of contests starting from a given date up to the latest contest
     '''
+    baseDf = auxiliary.readFromCsv('app/resources/DadosMegasena.csv')
 
     # If an starting date is given, select the subset of the dataframe
     if startDate:
-        tempDf = selectDateInterval(baseDf, startDate)
+        tempDf = dataframeAnalysis.selectDateInterval(baseDf, startDate)
     else:
         tempDf = baseDf
 
-    json = saveToJson(tempDf)
+    json = auxiliary.saveToJson(tempDf)
 
     return json
