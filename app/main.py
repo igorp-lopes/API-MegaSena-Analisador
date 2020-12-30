d1e4f27d9923cd654c96c2a89eba24f1c547c033
igorp-lopes <igorplopes16@gmail.com>
2020-12-30 01:25:09 -0300

    Criada função periódica que atualiza a base de dados

--- app/main.py
+++ app/main.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI
 from typing import Optional
+from fastapi_utils.tasks import repeat_every
+
 from app.routers import routeResults
 from app.routers import routeRecurrence
 from app.routers import routeDates
-
+from app.helpers import getData
+from app.helpers import auxiliary
 
 app = FastAPI()
+
+# Routine to update the results database daily
+
+
+@app.on_event('startup')
+@repeat_every(seconds=(60 * 60) * 24)
+def update_database():
+    """
+    Function that requests the results from the official database
+    and saves them for use in the API
+    """
+
+    # We obtain the data from the official database
+    df = getData.extractData()
+
+    # We save the dataframe for later use in the API
+    auxiliary.saveToCsv(df, 'app/resources')
+
 
 app.include_router(routeResults.router)
 app.include_router(routeRecurrence.router)
