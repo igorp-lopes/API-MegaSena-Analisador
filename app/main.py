74ccf540299851aae705278ed4eca704b59d50c0
igorp-lopes <igorplopes16@gmail.com>
2021-04-03 11:05:34 -0300

    Teste CORS

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://nextjs-mega-sena-analisador.vercel.app"],
     allow_origin_regex=os.environ.get("ALLOWED_ORIGINS"),
-    allow_methods=["*"],
+    allow_methods=[""],
     allow_headers=["*"],
     allow_credentials=True,
 )
